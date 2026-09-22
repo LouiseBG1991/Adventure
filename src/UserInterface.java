5c7ad24ba57efb8bbccf3f054c4dd59a96d0a595
AndersGraa <andersgraa@gmail.com>
2026-09-22 11:25:43 +0200

    Userinterface added

---
+++ src/UserInterface.java
@@ -0,0 +1,35 @@
+public class UserInterface {
+    public void StartAdventure(){
+
+        boolean activeAdventure = true;
+        IO.println("Welcome to the adventure game, you can move in 4 directions by typing n for north, e for east, s for south, w for west.");
+        while(activeAdventure){
+            String command = IO.readln("hvilken retning vil du gå?");
+            switch (command){
+                case "n" -> {
+                    IO.println("you go north");
+                }
+                case "s" -> {
+                    IO.println("you go south");
+                }
+                case "e" -> {
+                    IO.println("you go east");
+                }
+                case "w" -> {
+                    IO.println("you go west");
+                }
+                case "look" ->{
+                    IO.println("You look around");
+                }
+                case "Exit"-> {
+                    IO.println("goodbye");
+                    activeAdventure = false;
+                }
+                case "Help"->{
+                    IO.println("The commands are; n for north, e for east, w for west, s for south, Help for help, and Exit for exit.");
+                }
+            }
+        }
+
+    }
+}
