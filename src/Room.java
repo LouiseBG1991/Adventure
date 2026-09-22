ec7b5eaf7324bf40ef9d784c76d917375f8545c0
louis <louisebundgaard91@outlook.dk>
2026-09-22 11:49:09 +0200

    room class med metoder

--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,55 @@
 public class Room {
+    private String name;
+    private String description;
+    private Room north;
+    private Room east;
+    private Room south;
+    private Room west;
+
+    public Room (String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Room getNorth (){
+        return north;
+    }
+
+    public Room getEast () {
+        return east;
+    }
+
+    public Room getSouth () {
+        return south;
+    }
+
+    public Room getWest () {
+        return west;
+    }
+
+    public void setNorth (Room north) {
+        this.north = north;
+    }
+
+    public void setEast (Room east) {
+        this.east = east;
+    }
+
+    public void setWest (Room west) {
+        this.west = west;
+    }
+
+    public void setSouth (Room south) {
+        this.south = south;
+    }
+
+    public String getName() {
+        return name;
+
+    }
+
+    public String getDescription () {
+        return description;
+    }
+
 }
